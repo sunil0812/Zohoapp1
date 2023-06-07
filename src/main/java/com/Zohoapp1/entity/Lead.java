78b67b8e57f09b44f867c03332e30b2b677f05cf
sunil0812 <sunilkumarack2@gmail.com>
2023-06-07 22:25:01 +0530

    This is an ZohoApp_1 Simple project

--- src/main/java/com/Zohoapp1/entity/Lead.java
+++ src/main/java/com/Zohoapp1/entity/Lead.java
@@ -25,44 +25,52 @@ public class Lead {
 	private String paswd;
 	@Column(nullable=false,name="Gender")
 	private String gender;
-	
+
 	public long getSno() {
 		return sno;
 	}
+
 	public void setSno(long sno) {
 		this.sno = sno;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public long getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(long mobile) {
 		this.mobile = mobile;
 	}
+
 	public String getPaswd() {
 		return paswd;
 	}
+
 	public void setPaswd(String paswd) {
 		this.paswd = paswd;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
-	
-	
 }
